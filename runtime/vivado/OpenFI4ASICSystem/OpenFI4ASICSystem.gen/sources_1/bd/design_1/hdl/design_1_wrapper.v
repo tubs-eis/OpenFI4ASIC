61dc1e28d4b763e2d1b6ef306f987f4717296abb
Jasper Homann <jasper.homann@tu-bs.de>
2025-08-18 14:14:34 +0200

    Update Vivado project

--- runtime/vivado/OpenFI4ASICSystem/OpenFI4ASICSystem.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ runtime/vivado/OpenFI4ASICSystem/OpenFI4ASICSystem.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2024.2 (lin64) Build 5239630 Fri Nov 08 22:34:34 MST 2024
-//Date        : Fri Aug 15 11:08:55 2025
+//Date        : Mon Aug 18 10:20:05 2025
 //Host        : laptop-homann-25.eis.tu-bs.de running 64-bit Ubuntu 24.04.3 LTS
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
